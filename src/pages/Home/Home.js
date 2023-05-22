ab43784d9e205b39238e043203f300e9fd6391dd
Andrea Rotesi <andrearotesi@gmail.com>
2023-05-22 22:17:28 +0200

    Completed home page
    Added styling and content to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,43 @@
 import Card from '../../components/Card/Card'
+import './Home.scss';
+import portraitImg from '../../assets/images/portrait.png';
+import mountainImg from '../../assets/images/carezza-min.png';
+
 
 const Home = () => {
   const cardTitle = (
     <span>
       Software Engineer @<a href='https://www.aodocs.com/'><u>AODocs</u></a>
       <br/>
+      CS Student
+      <br/>
       Mentor
     </span>
   );
-  const cardSubtitle = 'CS Student, JS & CSS lover';
+  const cardSubtitle = 'JS & CSS lover';
 
   return (
-    <div>
-      <Card title={ cardTitle } subtitle={ cardSubtitle }/>
-      <h1>This is the home page</h1>
+    <div className='Home'>
+      <section className='center vert-spacing'>
+        <img src={ portraitImg } alt='Andrea Rotesi, SWE, CS Student'/>
+        <div className='intro__card'>
+          <Card title={ cardTitle } subtitle={ cardSubtitle }/>
+        </div>
+      </section>
+
+      <section className='center vert-spacing'>
+        <p className='about-me-info'>
+          <h3>Who am I?</h3>
+          Ciao! I'm Andrea, a Software Engineer with a vibrant passion for coding and design. 
+          I love to push my limits with coding projects and I study to pursue my dream of becoming an amazing coder!
+
+          <h3>What do I do?</h3>
+          Many things! After my nine-to-five, I work on my bachelor's in computer science or on my projects, and I
+          go on hikes. I love mountains and skiing, as well as cooking and wine tasting!
+        </p>
+        <img src={ mountainImg } alt='Carezza valley, Alto Adige, ski'/>
+      </section>
+      
     </div>
   );
 }
