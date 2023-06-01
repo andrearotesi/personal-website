216d41ae5717e0e22683e0e748eb78a4633b517d
Andrea Rotesi <andrearotesi@gmail.com>
2023-06-01 16:19:25 +0200

    Added link to cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Card.scss';
 
 const Card = (props) => {
-  const { title, subtitle } = props;
+  const { title, subtitle, link, linkLabel } = props;
 
   return (
     <div className="Card">
       <h1>{ title }</h1>
       <h3>{ subtitle }</h3>
+      { link && <a href={ link }>{ linkLabel ? linkLabel : link }</a> }
     </div>
   );
 }
